de8469232e6e4b9e5c448664624aeb4330d8f5c0
morrison29 <morrisoniniuko@gmail.com>
2026-05-20 10:09:42 +0100

    Frontend attempt

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 app.use('/todo', todoRouter);
